5ae0929e484905869dc61324f4b560dac552dd0e
cprm <https://github.com/SonerArslan2019/imalatlar.git>
2022-01-13 15:56:22 +0300

    model olusturuldu2

--- sld/forms.py
+++ sld/forms.py
@@ -6,19 +6,19 @@ from .models import SldModel
 class SldForm(forms.ModelForm):
     class Meta:
         model = SldModel
-        # fields = "__all__"
-        fields = [
-            'user',
-            'crm',
-            'firma_bilgileri',
-            'teslim_tarihi',
-            'cam',
-            'bitis',
-            'kapi_tipi',
-            'acilis_yonu',
-            'gecis_genisligi',
-            'gecis_yuksekligi',
-            'ustluk',
-            'toplam_yukseklik',
-            'mekanizma_genisligi',
-        ]
+        fields = "__all__"
+        # fields = [
+        #     'user',
+        #     'crm',
+        #     'firma_bilgileri',
+        #     'teslim_tarihi',
+        #     'cam',
+        #     'bitis',
+        #     'kapi_tipi',
+        #     'acilis_yonu',
+        #     'gecis_genisligi',
+        #     'gecis_yuksekligi',
+        #     'ustluk',
+        #     'toplam_yukseklik',
+        #     'mekanizma_genisligi',
+        # ]
